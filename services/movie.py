3ad23f85755fed6b2bc35dff1ababbd63ff01974
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-08-28 13:44:19 +0300

    fix get_movies func

--- services/movie.py
+++ services/movie.py
@@ -8,16 +8,11 @@ def get_movies(
         actors_ids: list[int] | None = None
 ) -> QuerySet:
     query_set = Movie.objects.all()
-    if genres_ids and actors_ids:
-        query_set = query_set.filter(
-            genres__id__in=genres_ids,
-            actors__id__in=actors_ids
-        )
-    elif genres_ids:
+    if genres_ids:
         query_set = query_set.filter(
             genres__id__in=genres_ids
         )
-    elif actors_ids:
+    if actors_ids:
         query_set = query_set.filter(
             actors__id__in=actors_ids
         )
